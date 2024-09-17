d3a9024f2e709c9d9c58ebf489425d16e43b60df
Martin <martin.kolberg@outlook.com>
2024-09-17 14:03:33 +0200

    Continued working on the bookings.js file

--- bonzaihotel/src/bookings.js
+++ bonzaihotel/src/bookings.js
@@ -15,6 +15,7 @@ const ROOMS_TABLE = process.env.ROOMS_TABLE;
 const app = initExpress();
 
 app.post('/bookings/:roomId', async (req, res) => {
+  console.log('request Body:', req.body);
   const body = req.body;
   const { roomId } = req.params;
   const checkIn = new Date(body.checkIn);
@@ -29,16 +30,41 @@ app.post('/bookings/:roomId', async (req, res) => {
   const roomCommand = new GetCommand(roomParams);
   const foundRoom = await docClient.send(roomCommand);
 
+  console.log('Found room:', foundRoom);
+
   if (!foundRoom.Item) {
     return res.status(404).json({ msg: 'No room found with that id' });
   }
 
+  const controlGuestAmount = (guests) => {
+    let maxGuests;
+    switch (foundRoom.Item.room) {
+      case 'single':
+        maxGuests = 1;
+        break;
+      case 'double':
+        maxGuests = 2;
+        break;
+      case 'suite':
+        maxGuests = 3;
+        break;
+      default:
+        maxGuests = 1;
+    }
+    if (guests > maxGuests) {
+      return res
+        .status(400)
+        .json({ msg: `Too many guests. Maximum allowed is ${maxGuests}` });
+    }
+  };
+
   const days = calculateDateDifference(checkIn, checkOut);
   const sum = Number(foundRoom.Item.price) * days;
   try {
+    controlGuestAmount(guests);
+
     const order = {
-      orderId: uuid(),
-      id: roomId,
+      id: uuid(),
       days: days,
       checkIn: checkIn.toISOString(),
       checkOut: checkOut.toISOString(),
@@ -50,11 +76,19 @@ app.post('/bookings/:roomId', async (req, res) => {
     const params = {
       TableName: BOOKINGS_TABLE,
       Item: order,
+      // Här tror jag att vi skall uppdatera BOOL till false
     };
-    console.log('params', params);
+    console.log('params', params.Item);
     const command = new PutCommand(params);
-    await docClient.send(command);
-    res.status(200).json({ msg: 'Room booked successfully', result: order });
+    const result = await docClient.send(command);
+
+    console.log('PutCommand Result:', result);
+
+    res.status(200).json({
+      msg: 'Room booked successfully',
+      orderId: order.id,
+      roomId: roomId,
+    });
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
